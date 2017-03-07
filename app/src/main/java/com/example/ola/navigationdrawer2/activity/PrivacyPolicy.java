f3560088d21565107286d0731359bd55f8a5a9df
Ola <ola@Olas-MacBook-Air.local>
2017-03-07 21:59:38 +0100

    sample working project

---
+++ app/src/main/java/com/example/ola/navigationdrawer2/activity/PrivacyPolicy.java
@@ -0,0 +1,23 @@
+package com.example.ola.navigationdrawer2.activity;
+
+import android.os.Handler;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.ola.navigationdrawer2.R;
+
+public class PrivacyPolicy extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_privacy_policy);
+    }
+}
